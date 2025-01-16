9773b9755cccee41d7643f21d0661f3b4225da12
javaprocoder <160300225+javaprocoder@users.noreply.github.com>
2025-01-16 08:34:11 +0530

    Made the Application to change automatically to light or Dark Mode.

---
+++ lib/core/themes/theme.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static final lightTheme = ThemeData(
+    primarySwatch: Colors.green,
+    visualDensity: VisualDensity.adaptivePlatformDensity,
+  );
+
+  static final darkTheme = ThemeData(
+    primarySwatch: Colors.deepOrange,
+    visualDensity: VisualDensity.adaptivePlatformDensity,
+  );
+}
--- lib/main.dart
+++ lib/main.dart
@@ -12,11 +12,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
+      theme: ThemeData(),
+      darkTheme: ThemeData.dark(),
+      themeMode: ThemeMode.system,
+      debugShowCheckedModeBanner: false,
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
