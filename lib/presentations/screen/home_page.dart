4de18f8f1f43229fcb3faea6a4846674773b7656
javaprocoder <160300225+javaprocoder@users.noreply.github.com>
2025-01-17 20:23:07 +0530

    Cleared Home Page Clutter.

--- lib/presentations/screen/home_page.dart
+++ lib/presentations/screen/home_page.dart
@@ -1,4 +1,3 @@
-// home_page.dart
 import 'package:flutter/material.dart';
 import 'package:smart_home_iot_control_center/presentations/date_picker_app.dart';
 
@@ -12,57 +11,41 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter += 2;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        centerTitle: true,
         title: Text(
           widget.title,
           style: const TextStyle(
-            fontSize: 23, // Increase the font size
+            fontSize: 24, // Increase the font size
             fontWeight: FontWeight.bold, // Make it bold
             color: Colors.white, // Set a custom color
             letterSpacing: 1.2, // Add some spacing between letters
           ),
         ),
       ),
-      body: Center(
+      body: const Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => const DatePickerApp()),
-                );
-              },
-              child: const Text("Open Date Picker"),
-            ),
+
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (context) => const DatePickerApp()),
+          );
+        },
+        // tooltip: 'Increment',
+        child: const Icon(Icons.date_range_outlined),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
