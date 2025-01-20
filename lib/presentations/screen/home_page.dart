50a5204cc18dcfbaa494f594d40b94925b9c7242
javaprocoder <160300225+javaprocoder@users.noreply.github.com>
2025-01-20 10:58:05 +0530

    Add Smart Devices with switches.

--- lib/presentations/screen/home_page.dart
+++ lib/presentations/screen/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:smart_home_iot_control_center/data/smart_device_box.dart';
 import 'package:smart_home_iot_control_center/presentations/date_picker_app.dart';
 
 class HomePage extends StatefulWidget {
@@ -94,18 +95,65 @@ class _HomePageState extends State<HomePage> {
                       color: Colors.lightBlue.shade600
                   ),
                 ),
-                const Text(
-                  "Hi Saad",
+                 Text(
+                  "Hi Saad,",
                   style: TextStyle(
                       fontWeight: FontWeight.w600,
                       fontSize: 22,
-                      color: Colors.purple
+                      color: Colors.purple[600]
                   ),
                 ),
               ],
             ),
           ),
 
+          const SizedBox(height: 25),
+          
+          const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 40.0),
+              child: Divider(
+                thickness: 1,
+                color: Color.fromARGB(255, 204, 204, 204),
+              ),
+          ),
+
+          const SizedBox(height: 25),
+
+        //   Smart Devices Grid
+          Padding(
+              padding: EdgeInsets.symmetric(horizontal: horizontalPadding),
+              child: const Text(
+                "Smart Devices",
+                style: TextStyle(
+                    fontWeight: FontWeight.w600,
+                    fontSize: 22,
+                    color: Colors.purple
+                ),
+              ),
+          ),
+
+          const SizedBox(height: 25),
+
+        //   Grid
+          Expanded(
+              child: GridView.builder(
+                itemCount: 4,
+                physics: const NeverScrollableScrollPhysics(),
+                padding: const EdgeInsets.symmetric(horizontal: 25),
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  childAspectRatio:1 / 1.3,
+                ),
+                itemBuilder: (context, index) {
+                  return SmartDeviceBox(
+                    smartDeviceName: mySmartDevices[index][0],
+                    iconPath: mySmartDevices[index][1],
+                    powerOn: mySmartDevices[index][2],
+                    onChanged: (value) => powerSwitchChanged(value, index),
+                  );
+                },
+              )
+          )
 
         ],
       )),
